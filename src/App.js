fa8b52084ee0f689f361d066692c823354782b27
Your Name <you@example.com>
2026-09-04 14:22:25 +0400

    Add build version

--- src/App.js
+++ src/App.js
@@ -15,9 +15,7 @@ function App() {
           Learn Jenkins on Udemy
         </a>
       </header>
-      <p>
-          Application version: 1
-      </p>
+      <p>Application version: {process.env.REACT_APP_VERSION || '1'}</p>
     </div>
   );
 }
